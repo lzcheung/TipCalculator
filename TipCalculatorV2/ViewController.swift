aa7343100b837906bf49e716acab4cbcb9e23bfd
Liang Cheung <lzcheung@calpoly.edu>
2017-05-09 21:32:31 -0700

    Initial Commit

---
+++ TipCalculatorV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculatorV2
+//
+//  Created by Liang Cheung on 5/9/17.
+//  Copyright © 2017 Liang Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TipCalculatorV2Tests/TipCalculatorV2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TipCalculatorV2Tests.swift
+//  TipCalculatorV2Tests
+//
+//  Created by Liang Cheung on 5/9/17.
+//  Copyright © 2017 Liang Cheung. All rights reserved.
+//
+
+import XCTest
+@testable import TipCalculatorV2
+
+class TipCalculatorV2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
